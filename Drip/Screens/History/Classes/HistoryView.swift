0ab795a89dde235568eaafb0d6420f4fb67489aa
Kaiman Mehmet <KaimanMehmet@gmail.com>
2021-02-17 03:28:57 +0000

    Collapsing calendar when scroll view is scrolled up.

--- Drip/Screens/History/Classes/HistoryView.swift
+++ Drip/Screens/History/Classes/HistoryView.swift
@@ -5,14 +5,33 @@ final class HistoryView: UIViewController, HistoryViewProtocol {
 
     var presenter: HistoryPresenterProtocol!
     @IBOutlet var calendar: FSCalendar!
+    @IBOutlet weak var calendarHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    fileprivate lazy var scopeGesture: UIPanGestureRecognizer = {
+        [unowned self] in
+        let panGesture = UIPanGestureRecognizer(target: self.calendar,
+                                                action: #selector(self.calendar.handleScopeGesture(_:)))
+        panGesture.delegate = self
+        panGesture.minimumNumberOfTouches = 1
+        panGesture.maximumNumberOfTouches = 2
+        return panGesture
+    }()
+
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter
+    }()
 
     override func viewDidLoad() {
         presenter.onViewDidLoad()
-        self.navigationItem.largeTitleDisplayMode = .automatic
-        self.navigationController?.navigationBar.prefersLargeTitles = true
         calendar.dataSource = self
         calendar.delegate = self
         calendar.register(CustomFSCell.self, forCellReuseIdentifier: "cell")
+
+        self.view.addGestureRecognizer(self.scopeGesture)
+        self.scrollView.panGestureRecognizer.require(toFail: self.scopeGesture)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -41,8 +60,29 @@ final class HistoryView: UIViewController, HistoryViewProtocol {
 
 }
 
+extension HistoryView: UIGestureRecognizerDelegate {
+
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        let shouldBegin = self.scrollView.contentOffset.y <= -self.scrollView.contentInset.top
+        if shouldBegin {
+            let velocity = self.scopeGesture.velocity(in: self.view)
+            switch self.calendar.scope {
+            case .month:
+                return velocity.y < 0
+            case .week:
+                return velocity.y > 0
+            @unknown default:
+                fatalError()
+            }
+        }
+        return shouldBegin
+    }
+
+}
+
 extension HistoryView: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
 
+    // populates cell
     func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
 //        let cell = calendar.dequeueReusableCell(withIdentifier: "cell", for: date, at: position) as! CustomFSCell
 
@@ -55,13 +95,30 @@ extension HistoryView: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDeleg
         if Calendar.current.isDate(date, inSameDayAs: Date()) {
             cell.ringView.setProgress(0.8)
         } else {
-            cell.ringView.setProgress(0)
+            let randomDouble = Double.random(in: 0...1) // remove me, placeholder
+
+            cell.ringView.setProgress(CGFloat(randomDouble))
         }
         return cell
     }
 
+    // did select
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        print("selected: \(date)")
+        print("did select date \(self.dateFormatter.string(from: date))")
+        let selectedDates = calendar.selectedDates.map({self.dateFormatter.string(from: $0)})
+        print("selected dates is \(selectedDates)")
+        if monthPosition == .next || monthPosition == .previous {
+            calendar.setCurrentPage(date, animated: true)
+        }
+    }
+
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        print("\(self.dateFormatter.string(from: calendar.currentPage))")
+    }
+
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        self.calendarHeightConstraint.constant = bounds.height
+        self.view.layoutIfNeeded()
     }
 
 }
--- Drip/Util/Resources/CustomFSCell.swift
+++ Drip/Util/Resources/CustomFSCell.swift
@@ -19,8 +19,6 @@ class CustomFSCell: FSCalendarCell {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-//        self.contentView.insertSubview(ProgressRingView, at: 0)
         let ringView = ProgressRingView()
         self.ringView = ringView
         ringView.setupRingView(progress: 0.5, colour: .dripMerged, shadowColour: .darkGray, lineWidth: 6)
@@ -35,17 +33,15 @@ class CustomFSCell: FSCalendarCell {
                                      width: self.contentView.bounds.width*0.9,
                                      height: self.contentView.bounds.height*0.9)
         ringView.center = contentView.center
-//        titleLabel.textColor = .green
-        self.titleLabel.frame = self.contentView.bounds
-        self.shapeLayer.path = getPath().cgPath
-//        self.shapeLayer.backgroundColor = UIColor.systemPink.cgColor
+        self.titleLabel.frame = self.contentView.bounds // Centers datelabel
+        self.shapeLayer.path = getPath().cgPath // this and next line center date selected ring
         self.shapeLayer.frame = CGRect(x: 0,
                                      y: 0,
                                      width: self.contentView.bounds.width*0.9,
                                      height: self.contentView.bounds.height*0.9)
     }
 
-    private func getPath() -> UIBezierPath {
+    private func getPath() -> UIBezierPath { // reduces size of selection ring
         let arcCenter = CGPoint(x: contentView.bounds.midX, y: contentView.bounds.midY)
         let radius = ringView.frame.height/3
         let circularPath = UIBezierPath(arcCenter: arcCenter,
