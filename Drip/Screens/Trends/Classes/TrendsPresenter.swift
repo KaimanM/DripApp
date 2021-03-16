42b30ae17cd0febc7f4d02df0ed897b4b3233c39
Kaiman Mehmet <KaimanMehmet@gmail.com>
2021-03-16 12:38:17 +0000

    Implements last 7 and last 30 days on trends

--- Drip/Screens/Trends/Classes/TrendsPresenter.swift
+++ Drip/Screens/Trends/Classes/TrendsPresenter.swift
@@ -11,19 +11,37 @@ final class TrendsPresenter: TrendsPresenterProtocol {
 
     var didLoadData: Bool = false
 
+    var lastWeekDate: Date {
+        let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
+        var components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date())
+        components.hour = 00
+        components.minute = 00
+        components.second = 00
+        let startDate = calendar.date(from: components)
+        return startDate!.addingTimeInterval(-(7*24*60*60))
+    }
+
+    var lastMonthDate: Date {
+        let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
+        var components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date())
+        components.hour = 00
+        components.minute = 00
+        components.second = 00
+        let startDate = calendar.date(from: components)
+        return startDate!.addingTimeInterval(-(30*24*60*60))
+    }
+
     init(view: TrendsViewProtocol) {
         self.view = view
     }
 
     func onViewDidAppear() {
-        print("Presenter onViewDidAppear firing correctly")
         view?.coreDataController.fetchDrinks()
         didLoadData = true
         view?.reloadData()
     }
 
     func onViewDidLoad() {
-        print("Presenter onViewDidLoad firing correctly")
         view?.updateTitle(title: "Trends")
     }
 
@@ -61,28 +79,54 @@ final class TrendsPresenter: TrendsPresenterProtocol {
         }
     }
 
-    // swiftlint:disable:next cyclomatic_complexity
+    // swiftlint:disable:next cyclomatic_complexity function_body_length
     func getDataForCell(section: Int, row: Int) -> String {
         if didLoadData == false { return "Loading" }
         switch section {
         case 0:
             switch row {
             case 0:
-                return getAverageDrinkAllTime()
+                return getAverageDrink(for: .allTime)
             case 1:
-                return getAverageDailyAllTime()
+                return getAverageDaily(for: .allTime)
             case 2:
-                return getBestDayAllTime()
+                return getBestDay(for: .allTime)
             case 3:
-                return getWorstDayAllTime()
+                return getWorstDay(for: .allTime)
             case 4:
                 return getCurrentStreak()
             case 5:
                 return getBestStreak()
             case 6:
-                return getFavouriteDrink()
+                return getFavouriteDrink(for: .allTime)
             case 7:
-                return getDailyDrinksAllTime()
+                return getDailyDrinks(for: .allTime)
+            default:
+                return "nil"
+            }
+        case 1:
+            switch row {
+            case 0:
+                return getAverageDrink(for: .last7Days)
+            case 1:
+                return getAverageDaily(for: .last7Days)
+            case 2:
+                return getFavouriteDrink(for: .last7Days)
+            case 3:
+                return getDailyDrinks(for: .last7Days)
+            default:
+                return "nil"
+            }
+        case 2:
+            switch row {
+            case 0:
+                return getAverageDrink(for: .last30Days)
+            case 1:
+                return getAverageDaily(for: .last30Days)
+            case 2:
+                return getFavouriteDrink(for: .last30Days)
+            case 3:
+                return getDailyDrinks(for: .last30Days)
             default:
                 return "nil"
             }
@@ -91,70 +135,73 @@ final class TrendsPresenter: TrendsPresenterProtocol {
         }
     }
 
-    func getAverageDrinkAllTime() -> String {
+    func getAverageDrink(for timescale: TimeScale) -> String {
         var total: Double = 0
         var amount: Double = 0
+        let drinkArray = getDrinksForRange(for: timescale)
 
-        if let coreDataController = view?.coreDataController {
-            for drink in coreDataController.allEntries {
-                total += drink.volume
-            }
-            amount = Double(coreDataController.allEntries.count)
+        for drink in drinkArray {
+            total += drink.volume
         }
+        amount = Double(drinkArray.count)
+
         guard !(total == 0 || amount == 0) else { return "-- ml"}
         return "\(Int(total/amount))ml"
     }
 
-    func getAverageDailyAllTime() -> String {
+    func getAverageDaily(for timescale: TimeScale) -> String {
         var total: Double = 0
         var days: Double = 0
         var date = Date(timeIntervalSince1970: 0)
-        if let coreDataController = view?.coreDataController {
-            for drink in coreDataController.allEntries.sorted(by: { $0.timeStamp > $1.timeStamp}) {
-                total += drink.volume
-                if !Calendar.current.isDate(drink.timeStamp, inSameDayAs: date) {
-                    days += 1
-                    date = drink.timeStamp
-                }
+        let drinkArray = getDrinksForRange(for: timescale)
+
+        for drink in drinkArray {
+            total += drink.volume
+            if !Calendar.current.isDate(drink.timeStamp, inSameDayAs: date) {
+                days += 1
+                date = drink.timeStamp
             }
         }
+
         guard !(total == 0 || days == 0) else { return "-- ml"}
         return "\(Int(total/days))ml"
     }
 
-    func getBestDayAllTime() -> String {
+    func getBestDay(for timescale: TimeScale) -> String {
         var total: Double = 0
         var best: Double = 0
         var date = Date(timeIntervalSince1970: 0)
-        if let coreDataController = view?.coreDataController {
-            for drink in coreDataController.allEntries {
-                if !Calendar.current.isDate(drink.timeStamp, inSameDayAs: date) {
-                    date = drink.timeStamp
-                    total = 0
-                }
-                total += drink.volume
-                if total > best { best = total }
+        let drinkArray = getDrinksForRange(for: timescale)
+
+        for drink in drinkArray {
+            if !Calendar.current.isDate(drink.timeStamp, inSameDayAs: date) {
+                date = drink.timeStamp
+                total = 0
             }
+            total += drink.volume
+            if total > best { best = total }
         }
+
         guard !(best == 0) else { return "-- ml"}
         return "\(Int(best))ml"
     }
 
-    func getWorstDayAllTime() -> String {
+    func getWorstDay(for timescale: TimeScale) -> String {
         var total: Double = Double.greatestFiniteMagnitude
         var worst: Double = Double.greatestFiniteMagnitude
         var date = Date(timeIntervalSince1970: 0)
-        if let coreDataController = view?.coreDataController {
-            for drink in coreDataController.allEntries {
-                if !Calendar.current.isDate(drink.timeStamp, inSameDayAs: date) {
-                    date = drink.timeStamp
-                    if total < worst { worst = total }
-                    total = 0
-                }
-                total += drink.volume
+        let drinkArray = getDrinksForRange(for: timescale)
+
+        for drink in drinkArray {
+            if !Calendar.current.isDate(drink.timeStamp, inSameDayAs: date) {
+                date = drink.timeStamp
+                if total < worst { worst = total }
+                total = 0
             }
-            if total < worst { worst = total }
+            total += drink.volume
         }
+        if total < worst { worst = total }
+
         guard !(worst > Double(Int.max)) else { return "-- ml"}
         return "\(Int(worst))ml"
     }
@@ -164,24 +211,24 @@ final class TrendsPresenter: TrendsPresenterProtocol {
         let goal: Double = 2000
         var date = Date()
         var streak = 0
+        let drinkArray = getDrinksForRange(for: .allTime)
 
-        if let coreDataController = view?.coreDataController {
-            for drink in coreDataController.allEntries.sorted(by: { $0.timeStamp > $1.timeStamp}) {
-                if Calendar.current.isDate(date, inSameDayAs: drink.timeStamp) {
-                    total += drink.volume
-                    if total > goal {
-                        streak += 1
-                        total = 0
-                        date = date.addingTimeInterval(-86400)
-                    }
-                }
-                // stops scanning through if more than days difference
-                if Date.daysBetween(start: date, end: drink.timeStamp) > 1 {
-                    break
+        for drink in drinkArray {
+            if Calendar.current.isDate(date, inSameDayAs: drink.timeStamp) {
+                total += drink.volume
+                if total >= goal {
+                    streak += 1
+                    total = 0
+                    date = date.addingTimeInterval(-86400)
                 }
             }
+            // stops scanning through if more than days difference
+            if Date.daysBetween(start: date, end: drink.timeStamp) > 1 {
+                break
+            }
         }
-        return "\(streak) days"
+
+        return "\(streak) day\(streak == 1 ? "" : "s")"
     }
 
     func getBestStreak() -> String {
@@ -190,44 +237,42 @@ final class TrendsPresenter: TrendsPresenterProtocol {
         var date = Date()
         var streak = 0
         var best = 0
+        let drinkArray = getDrinksForRange(for: .allTime)
 
-        if let coreDataController = view?.coreDataController {
-            for drink in coreDataController.allEntries.sorted(by: { $0.timeStamp > $1.timeStamp}) {
-                if Calendar.current.isDate(date, inSameDayAs: drink.timeStamp) {
-                    total += drink.volume
-                } else {
-                    date = drink.timeStamp
-                    streak = 0
-                    total = drink.volume
-                }
-                if total > goal {
-                    streak += 1
-                    total = 0
-                    date = date.addingTimeInterval(-86400)
-                }
-                if streak > best { best = streak }
-//                print("current streak is \(streak). Best is \(best)")
+        for drink in drinkArray {
+            if Calendar.current.isDate(date, inSameDayAs: drink.timeStamp) {
+                total += drink.volume
+            } else {
+                date = drink.timeStamp
+                streak = 0
+                total = drink.volume
             }
+            if total >= goal {
+                streak += 1
+                total = 0
+                date = date.addingTimeInterval(-86400)
+            }
+            if streak > best { best = streak }
         }
-        return "\(best) days"
+
+        return "\(best) day\(best == 1 ? "" : "s")"
     }
 
-    func getFavouriteDrink() -> String {
+    func getFavouriteDrink(for timescale: TimeScale) -> String {
         var drinkDictionary = [String: Int]()
-        if let coreDataController = view?.coreDataController {
-            for drink in coreDataController.allEntries {
-                if let count = drinkDictionary[drink.name] {
-                    drinkDictionary[drink.name] = count + 1
-                } else {
-                    drinkDictionary[drink.name] = 1
-                }
+        let drinkArray = getDrinksForRange(for: timescale)
+
+        for drink in drinkArray {
+            if let count = drinkDictionary[drink.name] {
+                drinkDictionary[drink.name] = count + 1
+            } else {
+                drinkDictionary[drink.name] = 1
             }
         }
 
         var mostCommonAmount = 0
-        var mostCommonName = "No Data"
+        var mostCommonName = "--"
         for key in drinkDictionary.keys {
-            print("\(key): \(drinkDictionary[key]!)")
             if drinkDictionary[key]! > mostCommonAmount {
                 mostCommonAmount = drinkDictionary[key]!
                 mostCommonName = key
@@ -236,20 +281,39 @@ final class TrendsPresenter: TrendsPresenterProtocol {
         return mostCommonName
     }
 
-    func getDailyDrinksAllTime() -> String {
+    func getDailyDrinks(for timescale: TimeScale) -> String {
         var days: Double = 0
         var drinkCount: Double = 0
         var date = Date(timeIntervalSince1970: 0)
-        if let coreDataController = view?.coreDataController {
-            drinkCount = Double(coreDataController.allEntries.count)
-            for drink in coreDataController.allEntries.sorted(by: { $0.timeStamp > $1.timeStamp}) {
-                if !Calendar.current.isDate(drink.timeStamp, inSameDayAs: date) {
-                    days += 1
-                    date = drink.timeStamp
-                }
+        let drinkArray = getDrinksForRange(for: timescale)
+
+        drinkCount = Double(drinkArray.count)
+        for drink in drinkArray {
+            if !Calendar.current.isDate(drink.timeStamp, inSameDayAs: date) {
+                days += 1
+                date = drink.timeStamp
             }
         }
+
         guard !(days == 0 || drinkCount == 0) else { return "-- drinks"}
         return String(format: "%.1f drinks", drinkCount/days)
+    }
+
+    func getDrinksForRange(for timeScale: TimeScale) -> [Drink] {
+        guard let coreDataController = view?.coreDataController else { return [] }
+        switch timeScale {
+        case .allTime:
+            return coreDataController.allEntries.sorted(by: { $0.timeStamp > $1.timeStamp})
+        case .last7Days:
+            return coreDataController.allEntries.filter({$0.timeStamp > lastWeekDate})
+                .sorted(by: { $0.timeStamp > $1.timeStamp})
+        case .last30Days:
+            return coreDataController.allEntries.filter({$0.timeStamp > lastMonthDate})
+                .sorted(by: { $0.timeStamp > $1.timeStamp})
+        }
+    }
+
+    enum TimeScale {
+        case allTime, last7Days, last30Days
     }
 }
