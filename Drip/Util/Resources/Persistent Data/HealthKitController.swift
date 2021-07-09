50d820258f033bc4c595c1700d1e9e01a8d4b4b1
Kaiman Mehmet <KaimanMehmet@gmail.com>
2021-07-09 01:53:53 +0100

    Cleaned up healthkit controller, wip

--- Drip/Util/Resources/Persistent Data/HealthKitController.swift
+++ Drip/Util/Resources/Persistent Data/HealthKitController.swift
@@ -11,91 +11,101 @@ class HealthKitController {
         }
     }()
 
-    var typesToShare : Set<HKSampleType> {
+    // Quantity type of dietary water for HealthKit
+    private let quantityType = HKQuantityType.quantityType(forIdentifier: .dietaryWater)!
+
+    // Set containing types to request HealthKit Permissions for, we use just water.
+    private var typesToShare : Set<HKSampleType> {
         let waterType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.dietaryWater)!
         return [waterType]
     }
 
+    // Checks authorisation status of just dietary water
     func checkAuthStatus(completion: @escaping (_ :HKAuthorizationStatus) -> Void) {
-        let status = healthStore?.authorizationStatus(for: HKQuantityType.quantityType(forIdentifier: .dietaryWater)!)
+        let status = healthStore?.authorizationStatus(for: quantityType)
         if let status = status {
             completion(status)
         }
     }
 
+    // Requests access in healthkit for dietary water
     func requestAccess(completion: @escaping (_ :Bool) -> Void) {
         healthStore?.requestAuthorization(toShare: typesToShare, read: nil) { granted, _ in
             completion(granted)
         }
     }
 
+    // Adds drink entry to health kit
     func addWaterDataToHealthStore(amount: Double, date: Date) {
-        let quantityType = HKQuantityType.quantityType(forIdentifier: .dietaryWater)
-
-        let quantityUnit = HKUnit(from: "l")
-
-        let quantityAmount = HKQuantity(unit: quantityUnit, doubleValue: amount/1000)
-
-        let now = date
-
-        let sample = HKQuantitySample(type: quantityType!, quantity: quantityAmount, start: now, end: now)
+        let sample = HKQuantitySample(type: quantityType, quantity: getLitreQuantity(amount), start: date, end: date)
 
         let correlationType = HKObjectType.correlationType(forIdentifier: HKCorrelationTypeIdentifier.food)
 
         let waterCorrelationForWaterAmount = HKCorrelation(type: correlationType!,
-                                                           start: now, end: now, objects: [sample])
+                                                           start: date, end: date, objects: [sample])
 
         healthStore?.save(waterCorrelationForWaterAmount, withCompletion: { success, error in
-            if (error != nil) {
-                print("failed to save")
-            } else {
-                print("saved successfully")
+            guard error == nil else {
+                print(error?.localizedDescription ?? "error failed to print")
+                return
             }
+            switch success {
+            case true:
+                print("Saved to HealthKit Successfully")
+            case false:
+                print("Failed to save to HealthKit")
+            }
+
         })
 
     }
 
-    func deleteEntryWithDate(date: Date = Date(), amount: Double = 0) {
+    // Deletes HealthKit Entry with given attributes
+    func deleteEntryWithAttributes(date: Date, amount: Double) {
         let startDate = date.addingTimeInterval(0)
         let endDate = date.addingTimeInterval(1)
-
-        let waterType = HKQuantityType.quantityType(forIdentifier: .dietaryWater)
 
         let datePredicate = HKSampleQuery.predicateForSamples(withStart: startDate,
                                                                end: endDate,
                                                                options: [])
 
-        let quantityUnit = HKUnit(from: "l")
-
-        let quantityAmount = HKQuantity(unit: quantityUnit, doubleValue: amount/1000)
-
-        let volumePredicate = HKSampleQuery.predicateForQuantitySamples(with: .equalTo, quantity: quantityAmount)
+        let volumePredicate = HKSampleQuery.predicateForQuantitySamples(with: .equalTo,
+                                                                        quantity: getLitreQuantity(amount))
 
         let combinedPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [datePredicate, volumePredicate])
 
-        let query = HKSampleQuery(sampleType: waterType!,
+        let query = HKSampleQuery(sampleType: quantityType,
                                   predicate: combinedPredicate,
                                   limit: 100,
-                                  sortDescriptors: nil) { query, results, error in
+                                  sortDescriptors: nil) { _, results, error in
 
-            if error != nil {
-                print(error?.localizedDescription)
-            } else {
-                if let first = results?.first as? HKObject {
-                    self.healthStore?.delete(first, withCompletion: { success, error in
-                        if success {
-                            print("successfully delete item from healthkit")
-                        } else {
-                            print("deleting failed: \(error)")
-                        }
-                    })
-                }
+            guard error == nil else {
+                print(error?.localizedDescription ?? "error failed to print")
+                return
+            }
+
+            if let healthKitEntry = results?.first {
+                self.healthStore?.delete(healthKitEntry, withCompletion: { success, error in
+                    guard error == nil else {
+                        print(error?.localizedDescription ?? "error failed to print")
+                        return
+                    }
+                    switch success {
+                    case true:
+                        print("Successfully removed entry from HealthKit")
+                    case false:
+                        print("Failed to remove entry from HealthKit")
+                    }
+                })
             }
         }
 
-
         healthStore?.execute(query)
+    }
 
+    // converts double ml amount to litres HKQuantity
+    private func getLitreQuantity(_ mlAmount: Double) -> HKQuantity {
+        return HKQuantity(unit: HKUnit(from: "l"), doubleValue: mlAmount/1000)
     }
 
 }
